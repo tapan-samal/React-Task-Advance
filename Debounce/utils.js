d0c5a8b7a5fe2e4e28036a5a04249478bc60a8cf
Tapan Samal <145225882+tapan-samal@users.noreply.github.com>
2024-09-08 10:35:50 +0530

    Update utils.js

--- Debounce/utils.js
+++ Debounce/utils.js
@@ -1,3 +1,4 @@
+//Simple version
 export const debounce = (func, delay) => {
     let timeoutId;
     return (...args) => {
@@ -10,3 +11,28 @@ export const debounce = (func, delay) => {
       }, delay);
     };
   };
+
+//Advance version: More flexible and versatile
+//Leading option execute the function immediately in initial stage
+//Trailing option controls the function should be executed after certain delay
+export const debounce = (func, delay, options = {}) => {
+  let timeoutId;
+  const { leading, trailing } = options;
+
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    if (leading && !timeoutId) {
+      func(...args);
+    }
+
+    timeoutId = setTimeout(() => {
+      if (trailing) {
+        func(...args);
+      }
+      timeoutId = null; // Clear the timeout ID to avoid unnecessary calls
+    }, delay);
+  };
+};
